95c35108de960eb9b8ae4e048eb98dcd0e3822ef
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-07-06 22:56:02 +0200

    Data

---
+++ main.js
@@ -0,0 +1,6 @@
+const jsonData = [
+  { id: 1, name: "Grace", sex: "Male" },
+  { id: 2, name: "Bahizi", sex: "Male" },
+  { id: 3, name: "Peter", sex: "Male" },
+  { id: 4, name: "Mapendano", sex: "Male" },
+];
